dee0bad4716eee1a01ece5558443f10c1573be78
heyuehan <632565074@qq.com>
2020-07-28 19:09:46 +0800

    删除不必要信息

--- src/main/java/john/study/p2w/ApplicationMain.java
+++ src/main/java/john/study/p2w/ApplicationMain.java
@@ -2,7 +2,7 @@ package john.study.p2w;
 
 public class ApplicationMain {
     public static void main(String[] args) {
-//        String res = new PdfToWord().pdftoword("C:\\Users\\JOHN\\Desktop\\convert\\每日易购平台计划书(1)(1).pdf");
+//        String res = new PdfToWord().pdftoword("*******.pdf");
 //        System.out.println(res);
 
         ExecuteServiceManager executeServiceManager = new ExecuteServiceManager();
